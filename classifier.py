85335fdb72fe17847a4d437ec70971ef5bfca2af
GodOfProbability <gopalsharma1193@gmail.com>
2016-07-08 17:22:25 +0530

    Add classifier

--- classifier.py
+++ classifier.py
@@ -3,25 +3,20 @@ import matplotlib
 matplotlib.use('Agg')
 from model import load_model
 from data_utils import data_utils
-
+from plotting import plot
+from train import train
 max_caption_len = 24
 vocab_size = 38
 batch_size = 3000
 nb_epoch = 2
-data_augmentation = True
+data_augmentation = False
 img_rows, img_cols = 32, 100
 
 X_train, X_test, Words_train, Words_test, y_train, y_test = data_utils()
 
 model = load_model()
 
-history = model.fit([X_train, Words_train], y_train, batch_size=32,
-          validation_data=([X_test, Words_test], y_test),
-          nb_epoch=1)
+past_history = train(model=model, data_augmentation=False, X_train=X_train, X_test=X_test,
+                     y_train=y_train, Words_train=Words_train, Words_test=Words_test)
 
-# train_model()
-# plot the results and training losses
-# test_model()
-# Save the model
-
-
+plot( past_history)
